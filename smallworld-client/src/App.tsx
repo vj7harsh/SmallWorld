7b349f2547ad213e7552bf50375550deee3015cd
vj7harsh <31484685+vj7harsh@users.noreply.github.com>
2025-09-05 17:06:53 -0700

    Add room selection homepage

--- smallworld-client/src/App.tsx
+++ smallworld-client/src/App.tsx
@@ -1,7 +1,20 @@
+import { useState } from 'react';
+import Home from './components/Home';
 import IrregularHexRegions from './components/IrregularHexRegions';
 
 function App() {
-  return <IrregularHexRegions />;
+  const [started, setStarted] = useState(false);
+
+  const handleStart = (mode: 'create' | 'join', players: number) => {
+    console.log(`Mode: ${mode}, players: ${players}`);
+    setStarted(true);
+  };
+
+  if (started) {
+    return <IrregularHexRegions />;
+  }
+
+  return <Home onStart={handleStart} />;
 }
 
 export default App;
